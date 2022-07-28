5a13e6eea1f8c0f66979944446d9911012e1cc15
JimWu <a0283137@gmail.com>
2022-07-28 10:03:18 +0800

    完成今日日期顯示及跑馬燈

---
+++ base.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+date_default_timezone_set("Asia/Taipei");
+
+
+
+
+class DB{
+    
+}
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-﻿<!DOCTYPE html
+﻿<?php
+include_once "base.php";
+?>
+
+<!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -20,7 +24,7 @@
     <iframe name="back" style="display:none;"></iframe>
     <div id="all">
         <div id="title">
-            00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
+            <?=date("m 月 d 號 l")?> | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
         <div id="title2">
 
         </div>
@@ -34,11 +38,13 @@
             </div>
             <div class="hal" id="main">
                 <div>
-
+					<marquee behavior="" direction="" style="width: 80%;display:inline-block;">
+						請民眾踴躍投稿電子報，讓電子報成為大家互相交流、分享的園地！詳見最新文章
+					</marquee>
                     <span style="width:18%; display:inline-block;">
                         <a href="?do=login">會員登入</a>
                     </span>
-                    <div class="">
+                    <div class="content">
                     </div>
                 </div>
             </div>
