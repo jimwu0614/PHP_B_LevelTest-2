a65cae67abdb61951d4d74fcdd6f3debc29252ca
JimWu <a0283137@gmail.com>
2022-08-01 11:32:07 +0800

    增加前一頁下一頁功能及人氣文章排序功能

--- front/news.php
+++ front/news.php
@@ -8,34 +8,46 @@
             <td width="20%"></td>
         </tr>
         <?php
-        $all=$News->math('count','id',['sh'=>1]);
-        $div=5;
-        $pages=ceil($all/$div);
-        $now=$_GET['p']??1;
-        $start=($now-1)*$div;
+        $all = $News->math('count', 'id', ['sh' => 1]);
+        $div = 5;
+        $pages = ceil($all / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * $div;
 
-        $rows=$News->all(['sh'=>1]," limit $start,$div");
+        $rows = $News->all(['sh' => 1], " limit $start,$div");
 
-        foreach($rows as $row){
+        foreach ($rows as $row) {
         ?>
-        <tr>
-            <td><?=$row['title'];?></td>
-            <td><?=mb_substr($row['text'],0,20);?>...</td>
-            <td></td>
-        </tr>
-        <?php 
+            <tr>
+                <td><?= $row['title']; ?></td>
+                <td><?= mb_substr($row['text'], 0, 20); ?>...</td>
+                <td></td>
+            </tr>
+        <?php
         }
 
         ?>
     </table>
     <div>
-        <?php 
+        <?php
+        //去前一頁的"<"
+        if (($now - 1) > 0) {
+            $p = $now - 1;
+            echo "<a href='?do=news&p={$p}'> &lt; </a>";
+        }
 
-        for($i=1;$i<=$pages;$i++){
-            $fontsize=($now==$i)?'24px':'18px';
+        for ($i = 1; $i <= $pages; $i++) {
+            $fontsize = ($now == $i) ? '24px' : '18px';
             echo "<a href='?do=news&p={$i}' style='font-size:{$fontsize}'> $i </a>";
         }
 
+        //去後一頁的">"
+        if (($now + 1) <= $pages) {
+            $p = $now + 1;
+            echo "<a href='?do=news&p={$p}'> &gt; </a>";
+        }
+
+
         ?>
     </div>
-</fieldset> 
+</fieldset>
