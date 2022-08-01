1459a8fd13744fce74e7ccffeb43cc1fe2280d5d
JimWu <a0283137@gmail.com>
2022-08-01 11:17:52 +0800

    完成最新文章顯示及分頁功能

--- front/pop.php
+++ front/pop.php
@@ -1,1 +1,11 @@
-人氣文章
+<fieldset>
+
+    <legend>目前位置：首頁 > 人氣文章區</legend>
+    <table>
+        <tr>
+            <td width="30%">標題</td>
+            <td width="50%">內容</td>
+            <td width="20%">人氣</td>
+        </tr>
+    </table>
+</fieldset>
